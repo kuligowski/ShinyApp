1a24948ab17eb2a9e555a5c85b9a9645586653ee
kuligowski <100proc3nt@gmail.com>
2014-06-21 16:25:35 +0200

    Update server.R

--- server.R
+++ server.R
@@ -2,7 +2,23 @@
 library(shiny)
 library(datasets)
 
-islandsData <- HairEyeColor
+characteristicsData <- HairEyeColor
 
 shinyServer(function(input, output) {
+  characteristiInput <- reactive({
+    switch(input$characteristic,
+           "Sex" = Sex,
+           "Eye" = Eye,
+           "Hair" = Hair)
+  })
+
+  # Generate a summary of the dataset
+  output$characteristicsSummary <- renderPrint({
+    summary(characteristicsData)
+  })
+
+  # Show the first "n" observations
+  output$table <- renderTable({
+    head(characteristiInput(), n = input$num)
+  })
 })
