501493f969787526258c89bc02be06c9e59cbae4
kuligowski <100proc3nt@gmail.com>
2014-06-21 16:25:26 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,6 +3,14 @@ library(shiny)
 
 shinyUI(projectPage(
   headerPanel("Developing data products project"),
-  sidebarPanel(),
-  mainPanel()
+  sidebarPanel(
+    selectInput("characteristic", "Choose a characteristic:", 
+                choices = c("Hair", "Eye", "Sex")),
+
+    numericInput("num", "Number of items to view:", 10)
+  ),
+  mainPanel(
+    characteristicsSummary("characteristicsSummary")
+    tableOutput("table")
+  )
 ))
