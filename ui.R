5bd4f6ad61fe27e5d507f544f11d9194b156bd0b
kuligowski <100proc3nt@gmail.com>
2014-06-24 03:20:00 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,16 +1,26 @@
 # 
 library(shiny)
 
-shinyUI(projectPage(
-  headerPanel("Developing data products project"),
+shinyUI(fluidPage(
+  titlePanel(h2(span("Hair", style = "color:blue"),"- "
+                , span("Eye", style = "color:green")
+                , " - ", span("Sex", style = "color:red")
+                , " characteristics relations")),
+  fluidRow(
+    column(10,
   sidebarPanel(
     selectInput("characteristic", "Choose a characteristic:", 
-                choices = c("Hair", "Eye", "Sex")),
-
-    numericInput("num", "Number of items to view:", 10)
-  ),
-  mainPanel(
-    characteristicsSummary("characteristicsSummary")
-    tableOutput("table")
+                choices = c("Hair", "Eye", "Sex"))
+  ))),
+  fluidRow(
+    h3("Summary:"),
+    verbatimTextOutput("characteristicsSummary"),
+    h3("Head: "),
+    verbatimTextOutput("characteristicsHead"),
+    plotOutput("plot1"),
+    plotOutput("plot3"),
+    plotOutput("plot4"),
+    plotOutput("plot2")
+    
   )
 ))
