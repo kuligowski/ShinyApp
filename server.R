45b5b65718de05ee3a4e003224a15666ae324f3a
kuligowski <100proc3nt@gmail.com>
2014-06-24 03:19:28 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,24 +1,50 @@
 #
 library(shiny)
 library(datasets)
+require(graphics)
 
 characteristicsData <- HairEyeColor
 
 shinyServer(function(input, output) {
-  characteristiInput <- reactive({
+  characteristicsInput <- reactive({
     switch(input$characteristic,
-           "Sex" = Sex,
-           "Eye" = Eye,
-           "Hair" = Hair)
+           "Sex" = 3,
+           "Eye" = 2,
+           "Hair" = 1)
   })
-
+  
+  output$plot1 <- renderPlot({
+    mosaicplot(characteristicsData)
+  })
+  
+  output$plot2 <- renderPlot({
+    if (characteristicsInput() != 1) {
+      x <- apply(characteristicsData, c(1, characteristicsInput()), sum)
+      mosaicplot(x, main = "Relation between hair and selected")
+    }
+  })
+  
+  output$plot3 <- renderPlot({
+    if (characteristicsInput() != 3) {
+      x <- apply(characteristicsData, c(characteristicsInput(), 3), sum)
+      mosaicplot(x, main = "Relation between selected and sex")
+    }
+  })
+  
+  output$plot4 <- renderPlot({
+    if (characteristicsInput() != 2) {
+      x <- apply(characteristicsData, c(2, characteristicsInput()), sum)
+      mosaicplot(x, main = "Relation between eye and selected")
+    }
+  })
+  
   # Generate a summary of the dataset
   output$characteristicsSummary <- renderPrint({
     summary(characteristicsData)
   })
-
-  # Show the first "n" observations
-  output$table <- renderTable({
-    head(characteristiInput(), n = input$num)
+  
+  output$characteristicsHead <- renderPrint({
+    head(characteristicsData)
   })
-})
+  
+})                                                                                                                          
